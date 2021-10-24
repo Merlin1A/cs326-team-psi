cbfb90d54a71d7aa8ed8537a8d4347c3eb252286
Merlin1A <jbrookins@umass.edu>
2021-10-24 18:42:21 -0400

    Fix port bug

--- app.js
+++ app.js
@@ -5,4 +5,7 @@ var app = express();
 // Render static files
 app.use(express.static('public'));
 // Port website will run on
-app.listen($DYNO);
+
+const PORT = process.env.PORT || 80;
+
+app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
