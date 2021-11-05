e3b8d61f1b5c867ba1cbe010772c9cd4c220f1af
sperek27 <sperek27@gmail.com>
2021-11-05 17:12:15 -0400

    added skeleton of server code

---
+++ server/index.js
@@ -0,0 +1,81 @@
+'use strict';
+import * as http from 'http';
+import * as fs from 'fs';
+import express from express;
+
+const JSONfile = './persistent.json';
+const port = 3000;
+const headerText = {'Content-Type': 'application/json;charset=utf-8'};
+const app = express();
+let memory = {};
+
+function reload(filename) {
+    if (fs.existsSync(filename)) {
+        memory = JSON.parse(fs.readFileSync(JSONfile));
+    }
+    else {
+        memory = {};
+    }
+}
+
+reload(JSONfile);
+app.use(express.json()); // lets you handle JSON input
+
+app.get('/account', (req, res) => {
+  // TODO
+  res.send()
+});
+
+app.get('/account/login', (req, res) => {
+  // TODO
+  res.send()
+});
+
+app.post('/account/update', (req, res) => {
+  // TODO
+  res.send()
+});
+
+app.post('/account/delete', (req, res) => {
+  // TODO
+  res.send()
+});
+
+app.post('/account/create', (req, res) => {
+  // TODO
+  res.send()
+});
+
+app.get('/courses', (req, res) => {
+  // TODO
+  res.send()
+});
+
+app.get('/course/reviews', (req, res) => {
+  // TODO
+  res.send()
+});
+
+app.post('/course/review/new', (req, res) => {
+  // TODO
+  res.send()
+});
+
+app.post('/course/review/vote', (req, res) => {
+  // TODO
+  res.send()
+});
+
+app.post('/course/survey/new', (req, res) => {
+  // TODO
+  res.send()
+});
+
+
+app.get('*', (req, res) => {
+  res.send('NO FOOL');
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
