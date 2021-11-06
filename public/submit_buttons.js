cee89603eb214801a6a43ec1f80ffbc0f5f16ff9
lynnli0 <lynnli032@gmail.com>
2021-11-06 19:56:23 -0400

    testing for form completion in create account

--- public/submit_buttons.js
+++ public/submit_buttons.js
@@ -4,7 +4,7 @@ function validateAcc(){
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
     let retype = document.getElementById('retype-password').value;
-    if(email.length === 0 || name === null || username === null || password === null || retype === null){
+    if(email.length === 0 || name === "Name" || username === "Username" || password === "Passoword" || retype === "Retype password"){
         alert("Please fill in all fields in order to sign up.")
     } 
     else {
