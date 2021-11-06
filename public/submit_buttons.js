54909a17734f638831fbf747f3a3ac379e55b6d8
lynnli0 <lynnli032@gmail.com>
2021-11-06 19:49:14 -0400

    added in alerts for form completion in account sign up

---
+++ public/submit_buttons.js
@@ -0,0 +1,15 @@
+function validateAcc(){
+    let email = document.getElementById('email').value;
+    let name = document.getElementById('name').value;
+    let username = document.getElementById('username').value;
+    let password = document.getElementById('password').value;
+    let retype = document.getElementById('retype-password').value;
+    if(email === null || name === null || username === null || password === null || retype === null){
+        alert("Please fill in all fields in order to sign up.")
+    } 
+    else {
+        alert("You are now signed up!");
+    }
+}
+
+document.getElementById('sign-up').addEventListener('click', validateAcc);
