beffab1ef2f4124ec217f370b64fdd88043f3c4f
Merlin1A <jbrookins@umass.edu>
2021-11-12 18:13:30 -0500

    Fix test code

--- server/index.js
+++ server/index.js
@@ -11,19 +11,33 @@ const app = express();
 let memory = {};
 
 // Test MongoDB
-import * as mongodb from 'mongodb';
+import { MongoClient } from "mongodb";
 const uri = process.env.MONGODB_URI;
-const client = new mongodb.MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+const client = new MongoClient(uri);
 console.log(uri);
-client.connect(err => {
-  const collection = client.db("sample_airbnb").collection("listingsAndReviews");
-  // perform actions on the collection object
-  const query = {"_id": "10006546"};
-  const document = collection.findOne(query);
-  console.log(document);
 
-  client.close();
-});
+async function run() {
+  try {
+    await client.connect();
+    const database = client.db("sample_mflix");
+    const movies = database.collection("movies");
+    // Query for a movie that has the title 'The Room'
+    const query = { title: "The Room" };
+    const options = {
+      // sort matched documents in descending order by rating
+      sort: { "imdb.rating": -1 },
+      // Include only the `title` and `imdb` fields in the returned document
+      projection: { _id: 0, title: 1, imdb: 1 },
+    };
+    const movie = await movies.findOne(query, options);
+    // since this method returns the matched document, not a cursor, print it directly
+    console.log(movie);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
 // Test MongoDB
 
 function reload(filename) {
