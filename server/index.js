dca713d3bae34120a90cd62b09f0f3dc7b29b14b
Merlin1A <jbrookins@umass.edu>
2021-11-12 18:17:54 -0500

    Edit query parameter

--- server/index.js
+++ server/index.js
@@ -21,15 +21,9 @@ async function run() {
     await client.connect();
     const database = client.db("sample_mflix");
     const movies = database.collection("movies");
-    // Query for a movie that has the title 'The Room'
-    const query = { title: "The Room" };
-    const options = {
-      // sort matched documents in descending order by rating
-      sort: { "imdb.rating": -1 },
-      // Include only the `title` and `imdb` fields in the returned document
-      projection: { _id: 0, title: 1, imdb: 1 },
-    };
-    const movie = await movies.findOne(query, options);
+    const query = { title: "Blacksmith Scene" };
+
+    const movie = await movies.findOne(query);
     // since this method returns the matched document, not a cursor, print it directly
     console.log(movie);
   } finally {
