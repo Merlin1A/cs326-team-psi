c0a6c138543ae19fe88efefd5f3a960f15d04c39
sperek27 <sperek27@gmail.com>
2021-11-24 15:04:00 -0500

    added new function to reviews.js

--- server/reviews.js
+++ server/reviews.js
@@ -9,7 +9,20 @@ export async function fetchReviews(coursecode) {
         const database = client.db("course");
         const reviews = database.collection("reviews");
         const ans = await reviews.find({"course_code" : coursecode}).toArray();
-        return ans["reviews"];
+        return ans;
+    } finally {
+        await client.close();
+    }
+}
+
+export async function fetchReview(uid) {
+    const client = new MongoClient(uri);
+    try {
+        await client.connect();
+        const database = client.db("course");
+        const reviews = database.collection("reviews");
+        const ans = await reviews.findOne({"uid" : uid});
+        return ans;
     } finally {
         await client.close();
     }
