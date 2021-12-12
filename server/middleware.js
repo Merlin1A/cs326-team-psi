e08ef4e29c2358eeb5c9192a5d5612897735020c
Merlin1A <jbrookins@umass.edu>
2021-12-12 12:49:37 -0500

    test

--- server/middleware.js
+++ server/middleware.js
@@ -1,5 +1,4 @@
 import { createAccount, deleteAccount, verifyAccount, addCode, getCode } from "./accounts.js";
-import { verificationEmail } from "./email.js";
 
 // Middleware functions
 
@@ -54,4 +53,52 @@ export function deleteUser(req, res, next){
 export const asyncMiddleware = fn => (req, res, next) => {
     Promise.resolve(fn(req, res, next)).catch(next);
 }
+
+// test
+
+import nodemailer from 'nodemailer';
+import { createRequire } from 'module';
+
+const require = createRequire(import.meta.url);
+let pass;
+let secrets;
+if (!process.env.MONGODB_URI) {
+    secrets = require('./secrets.json');
+    pass = secrets.support_email_pass;
+}
+else {
+    pass = process.env.SUPPORT_EMAIL_PASS;
+}
+
+/**
+ * This function sends a verification email 
+ * @param {string} recipient recipient's email address
+ * @param {number} code a five digit code to verify their account
+ * @returns nothing
+ */
+function verificationEmail(recipient, code){
+    const transporter = nodemailer.createTransport({
+        host: 'mail.privateemail.com',
+        port: 587,
+        secureConnection: true,
+        auth: {
+            user: 'support@courseoverflow.app',
+            pass: pass
+        }
+    });
+             
+    const mailOptions = { 
+        from: 'support@courseoverflow.app', 
+        to: recipient, 
+        subject: 'Email Confirmation', 
+        text: 'Hello,\n\n' 
+          + 'Please enter the following code in the verification page \n\n' 
+          + code
+    };
+    
+    transporter.sendMail(mailOptions, function (err) {
+        if (err) { console.log(err.message); }        
+    });
+}
+
 
