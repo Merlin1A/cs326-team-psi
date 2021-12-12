109342bea53ba5ace42417cdec9e03a0e44ed484
Merlin1A <jbrookins@umass.edu>
2021-12-12 12:53:11 -0500

    Extra file

--- app.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use(express.static('public'));
-
-const PORT = process.env.PORT || 80;
-
-app.listen(PORT, () => console.log(`Server is listening on port ${PORT}...`));
